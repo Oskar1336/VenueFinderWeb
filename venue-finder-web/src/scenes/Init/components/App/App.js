ec34a04986247f54f5417d5ab02718ab325eddf9
Oskar Georgsson <oskar.georgsson@gmail.com>
2018-02-28 12:21:25 +0100

    Init

--- venue-finder-web/src/scenes/Init/components/App/App.js
+++ venue-finder-web/src/scenes/Init/components/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <h1>Hello, World!</h1>
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
