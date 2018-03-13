00ddda5b6cc6c1cbac8aef3ec8b0d70bf4671620
ag7829 <ponta45@hotmail.com>
2018-03-13 16:34:33 +0100

    Margin top pic

--- venue-finder-web/src/scenes/Init/components/EventList/components/EventList.js
+++ venue-finder-web/src/scenes/Init/components/EventList/components/EventList.js
@@ -40,7 +40,7 @@ class VenueInfo extends React.Component {
 		return(
 			<div className="row">
 				<div className="card col-lg-6 offset-lg-3 text-white bg-dark">
-				  <img className="card-img-top" src={concertPic} alt="Card image cap"/>
+				  <img className="card-img-top" id="concertLogo" src={concertPic} alt="Card image cap"/>
 				  <div className="card-body">
 				    <h5 className="card-title">{this.props.events.title}</h5>
 				    <p className="card-text">{this.props.events.location}</p>
