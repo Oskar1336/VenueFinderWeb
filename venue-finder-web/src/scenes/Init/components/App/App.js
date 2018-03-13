a601458a82c227356cafb4df081ec6de005c56c5
LinusHakansson <linus.haknsn@gmail.com>
2018-03-13 12:59:13 +0100

    SearchComponent

--- venue-finder-web/src/scenes/Init/components/App/App.js
+++ venue-finder-web/src/scenes/Init/components/App/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
 import logo from './images/logo.svg';
 import './App.css';
+import SearchField from '../Search/components/SearchField/SearchField.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <h1>Hello, World!</h1>
-
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Welcome to React</p>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+        <SearchField/>
+      </div>  
     );
   }
 }
--- venue-finder-web/src/scenes/Init/components/Search/components/SearchField/SearchField.js
+++ venue-finder-web/src/scenes/Init/components/Search/components/SearchField/SearchField.js
@@ -1,3 +1,74 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import VenueContainer from '../EventList/components/EventList.js';
+
+
+export default class SearchField extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.state =  {
+			city : '',
+			venueList: []
+		};
+
+		this.updateCity = this.updateCity.bind(this);
+		this.fetchResults = this.fetchResults.bind(this);
+	}
+	render(){
+		return(
+				<div className = "row">
+					<div className="col-lg-6 offset-lg-3">
+						<div className="input-group mb-3">
+			  				<input type="text" value={this.state.city} onChange= {this.updateCity} className="form-control" placeholder={this.props.hintText} aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+			  				<div className="input-group-append">
+			    				<button className="btn btn-outline-secondary" onClick={this.fetchResults} type="button">{this.props.btnText}</button>
+			  				</div>
+						</div>
+					</div>	
+				</div>
+				<VenueContainer events={this.state.venueList}/>
+			</div>		
+		);
+	}
+
+	updateCity(evt){
+		this.setState({
+			city : evt.target.value,
+		});
+			
+	}
+
+	fetchResults(){
+		console.log(this.state.city);
+		axios.get("http://api.songkick.com/api/3.0/events.json?location=geo:55.6,13.0&apikey=zPpYhc36BMCR85fT")
+			.then(function(response){
+				var eventList = [];
+				console.log(response);
+				for(var i = 0; i < response.resultsPage.results.event.length; i++){
+					var event = {
+						title: response.resultsPage.results.event[i].displayName,
+						location: response.resultsPage.results.event[i].venue.displayName,
+						link: response.resultsPage.results.event[i].uri,
+						time: response.resultsPage.results.event[i].start.time
+					};
+					eventList.push(event);
+				}
+				this.setState({
+					venueList: [...this.state.venueList, eventList]
+					});
+
+			})
+			.catch(function(response){
+				console.log(response);
+			});
+	}
+
+}
+
+SearchField.defaultProps = {
+		hintText: "Choose city",
+		btnText: "Search"		
+}
 
 
